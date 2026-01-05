c00b3c5c208a498eef83d4b06b081204da81a088
TGS <107330609+the-game-stoner@users.noreply.github.com>
2026-01-04 21:24:52 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: "https://example.com", // change later to your real domain
+  output: "server",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
