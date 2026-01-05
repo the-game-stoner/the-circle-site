3f7e02cce229269d6c0fed869d63090b13e2f00c
TGS <107330609+the-game-stoner@users.noreply.github.com>
2026-01-04 22:18:56 -0500

    Update SITE_DESCRIPTION for clarity

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "The-Circle Blog";
-export const SITE_DESCRIPTION = "Welcome to my The-Circle!";
+export const SITE_DESCRIPTION = "Welcome to The-Circle!";
